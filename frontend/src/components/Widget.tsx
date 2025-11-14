4bb04c1e558dcc0fb55cf08e8bba983fcc0293ed
makz333 <216419165+makz333@users.noreply.github.com>
2025-11-14 11:18:48 +0100

    Widget Design und Hover Effekt angepasst

--- frontend/src/components/Widget.tsx
+++ frontend/src/components/Widget.tsx
@@ -9,7 +9,7 @@ interface WidgetProps {
 
 const Widget: React.FC<WidgetProps> = ({ title, icon, color }) => {
   return (
-    <div className="widget" style={{ backgroundColor: color }}>
+    <div className="widget" style={{ '--border-color': color } as React.CSSProperties & { '--border-color': string }}>
       <div className="widget-icon">{icon}</div>
       <div className="widget-title">{title}</div>
     </div>
